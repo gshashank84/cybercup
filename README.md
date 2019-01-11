3531ed1a8a3a1e451d8f749dd7a7b1de8d25fdd8
gshashank84 <gshashank84@gmail.com>
2019-01-11 16:54:38 +0530

    Upload of all files

---
+++ README.md
@@ -0,0 +1,15 @@
+## Online Web Deployment of Machine Learning Trained Classifier
+#### The classifier make predictions of Chest-Xray Images to predict PNEUMONIA disease
+[cybercup.app.render.com](https://cybercup.app.render.com)
+
+#### Here is the [link](https://github.com/gshashank84/cybercup/blob/master/CyberCup.ipynb) to the Jupyter Notebook (For Training the model)
+```python
+jupyter notebook CyberCup.ipynb
+```
+
+### Chest-xray Images for Normal and Pneumonia:
+#### Normal
+![Normal](https://github.com/gshashank84/cybercup/blob/master/rsz_normal0.jpg)
+
+#### Pneumonia
+![Pneumoonia](https://github.com/gshashank84/cybercup/blob/master/rsz_pneumonia1331.jpg)
---
+++ app/server.py
@@ -0,0 +1,56 @@
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn, aiohttp, asyncio
+from io import BytesIO
+
+from fastai import *
+from fastai.vision import *
+
+model_file_url = 'https://drive.google.com/uc?export=download&id=1DYL1NDeS24L7NMpRItYi2d8jewh_joS2'
+model_file_name = 'model'
+
+classes = ['NORMAL','PNEUMONIA']
+path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f: f.write(data)
+
+async def setup_learner():
+    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
+    data_bunch = ImageDataBunch.single_from_classes(path, classes,
+        tfms=get_transforms())
+    learn = create_cnn(data_bunch, models.resnet34, pretrained=False)
+    learn.load(model_file_name)
+    return learn
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+@app.route('/')
+def index(request):
+    html = path/'view'/'index.html'
+    return HTMLResponse(html.open().read())
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    data = await request.form()
+    img_bytes = await (data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=5042)
+
